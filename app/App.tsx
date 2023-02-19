75b39b2f2f4e7eddcdb975106dc210cfd60459d8
Ben <benknight135@gmail.com>
2023-02-19 16:47:36 +0000

    add parent package for running app & api

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,63 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { ActivityIndicator, Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import BankConnectButton from './components/BankConnectButton'
+import BankConnectButton from './components/BankConnectButton';
+
+type APIVersion = {
+  version: string;
+};
 
 export default function App() {
+  const [isLoadingAPIVersion, setLoadingAPIVersion] = useState(true);
+  const [apiVersion, setAPIVersion] = useState<APIVersion>({version: "0.0.0.0"});
+
+  const tinkClientId: string = "e510fbadcd714f7ca5ef141d4923f6c1"
+  var tinkRedirectURI: string = "";
+  if (Platform.OS === 'ios') {
+    // TODO use mobile app uri callback
+    // https://stackoverflow.com/questions/17427707/whats-the-right-oauth-2-0-flow-for-a-mobile-app
+    tinkRedirectURI = "";
+  } else if (Platform.OS === 'android') {
+    // TODO use mobile app uri callback
+    // https://stackoverflow.com/questions/17427707/whats-the-right-oauth-2-0-flow-for-a-mobile-app
+    tinkRedirectURI = "";
+  } else if (Platform.OS === 'web') {
+    tinkRedirectURI = "https%3A%2F%2Fconsole.tink.com%2Fcallback";
+  }
+
+  const apiBaseURl = "http://localhost:7071/api"
+  const apiVersionEndpoint = apiBaseURl + "/Version"
+
+  const getAPIVersion = async () => {
+    try {
+      const response = await fetch(apiVersionEndpoint);
+      const json = await response.json();
+      setAPIVersion(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoadingAPIVersion(false);
+    }
+  };
+
+  useEffect(() => {
+    getAPIVersion();
+  }, []);
+  
   return (
     <View style={styles.container}>
       <Text>Knight Life</Text>
-      <BankConnectButton title="Connect Bank"/>
+      <BankConnectButton
+        title='Connect Bank'
+        tinkClientId={tinkClientId}
+        tinkRedirectUri={tinkRedirectURI}
+      />
+      {isLoadingAPIVersion ? (
+        <ActivityIndicator />
+      ) : (
+        <Text>{apiVersion.version}</Text>
+      )}
       <StatusBar style="auto" />
     </View>
   );
--- app/components/BankConnectButton.tsx
+++ app/components/BankConnectButton.tsx
@@ -3,12 +3,12 @@ import LinkButton from './LinkButton'
 
 type BankConnectButtonProps = {
   title: string;
+  tinkClientId: string;
+  tinkRedirectUri: string;
 };
 
-const BankConnectButton = ({title}: BankConnectButtonProps) => {
-    const tink_client_id = "e510fbadcd714f7ca5ef141d4923f6c1"
-    const tink_redirect_uri = "https%3A%2F%2Fconsole.tink.com%2Fcallback"
-    const url = "https://link.tink.com/1.0/transactions/connect-accounts/?client_id=" + tink_client_id + "&redirect_uri=" + tink_redirect_uri + "&market=GB&locale=en_US"
+const BankConnectButton = ({title, tinkClientId, tinkRedirectUri}: BankConnectButtonProps) => {
+    const url = "https://link.tink.com/1.0/transactions/connect-accounts/?client_id=" + tinkClientId + "&redirect_uri=" + tinkRedirectUri + "&market=GB&locale=en_US"
 
   return <LinkButton title={title} url={url} />;
 };
