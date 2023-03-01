1d9ee38f83d3622ff9e0c87c70d1077aa4d231cd
Ben <benknight135@gmail.com>
2023-03-01 20:08:43 +0000

    remove api key from default env

--- app/utils/env.tsx
+++ app/utils/env.tsx
@@ -1,6 +1,6 @@
 export default {
-    API_BASE_URL: process.env.API_BASE_URL || "https://knightlife-api.azurewebsites.net/api",
-    API_ACCESS_KEY: process.env.API_VERSION_ACCESS_KEY || "tRW2dtKdOKWUdHN2rkLV3G-i653rPh6BTkN6TuoQ8iz1AzFu1QWvhw==",
+    API_BASE_URL: process.env.API_BASE_URL || "",
+    API_ACCESS_KEY: process.env.API_VERSION_ACCESS_KEY || "",
     OPEN_BANKING_PROVIDER: process.env.OPEN_BANKING_PROVIDER || "truelayer",
     OPEN_BANKING_USE_SANDBOX: process.env.OPEN_BANKING_USE_SANDBOX || true
 }
