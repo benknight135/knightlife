224e4353504176ebdaff1af6ca1b64bdc4f4cae0
Ben <benknight135@gmail.com>
2023-03-03 17:42:01 +0000

    fix default api base url

--- app/utils/env.tsx
+++ app/utils/env.tsx
@@ -1,5 +1,5 @@
 export default {
-    API_BASE_URL: process.env.API_BASE_URL || "",
+    API_BASE_URL: process.env.API_BASE_URL || "/api",
     API_ACCESS_KEY: process.env.API_VERSION_ACCESS_KEY || "",
     OPEN_BANKING_PROVIDER: process.env.OPEN_BANKING_PROVIDER || "truelayer",
     OPEN_BANKING_USE_SANDBOX: process.env.OPEN_BANKING_USE_SANDBOX || true
